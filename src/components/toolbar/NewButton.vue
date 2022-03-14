1b4335fa601badb744e2d40c93b29f82b91068ab
Sedrik Staack <sedrik.staack@sidion.de>
2022-03-14 18:35:36 +0100

    NewButton link

--- src/components/toolbar/NewButton.vue
+++ src/components/toolbar/NewButton.vue
@@ -7,7 +7,7 @@
 
       <v-list>
         <v-list-item-group color="primary">
-          <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item v-for="(item, i) in items" :key="i" link :to="item.link">
             <v-list-item-icon>
               <v-icon v-text="item.icon"></v-icon>
             </v-list-item-icon>
@@ -26,11 +26,11 @@ export default {
     name: "NewButton",
     data: () => ({
         items: [
-            { text: "Location", icon: "mdi-clock" },
-            { text: "Device", icon: "mdi-account" },
-            { text: "Action", icon: "mdi-flag" },
-            { text: "Type", icon: "mdi-flag" },
-            { text: "Data type", icon: "mdi-flag" },
+            { text: "Location", icon: "mdi-clock", link: '/creator/create/0/location' },
+            { text: "Device", icon: "mdi-account", link: '/creator/create/0/device' },
+            { text: "Action", icon: "mdi-flag", link: '/creator/create/0/action' },
+            { text: "Type", icon: "mdi-flag", link: '/creator/create/0/type' },
+            { text: "Data type", icon: "mdi-flag", link: '/creator/create/0/datatype' },
         ],
     }),
 };
