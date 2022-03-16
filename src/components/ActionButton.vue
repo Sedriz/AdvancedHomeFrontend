f2cf314ad37519b7919a42096930c9f579516025
Sedrik Staack <sedrik.staack@sidion.de>
2022-03-16 09:27:39 +0100

    Action button

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="mx-5">
+    <v-row>
+      <v-btn
+          depressed
+          elevation="13"
+          fab
+          class="mx-auto mb-2"
+          x-large
+          :color="color"
+      >
+        <v-icon x-large :color="getButtonColor">
+          {{ getButtonIcon }}
+        </v-icon>
+      </v-btn>
+    </v-row>
+    <v-row>
+      <span class="mx-auto text-wrap overflow-hidden btn-title">
+        {{ displayData.button.title }}
+      </span>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    displayData: {
+      type: Object,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  computed: {
+    getButtonIcon() {
+      let activeState = this.displayData.device.activeState;
+      if (activeState === true) {
+        return this.displayData.button.iconActive;
+      }
+      return this.displayData.button.iconInactive;
+    },
+    getButtonColor() {
+      let activeState = this.displayData.device.activeState;
+      let color = "error";
+      if (activeState === 1) {
+        color = 'active';
+      } else if (activeState === 0) {
+        color = "textPrimary";
+      }
+      return color;
+    },
+  },
+};
+</script>
+<style scoped>
+.btn-size {
+  width: 8em !important;
+  height: 6em !important;
+}
+.btn-title {
+  width: 7em !important;
+  height: 3em !important;
+}
+</style>
--- src/components/home/DeviceActionListView.vue
+++ src/components/home/DeviceActionListView.vue
@@ -1,20 +1,19 @@
 <template>
   <v-container>
     <div class="d-flex flex-wrap justify-center">
-      <DeviceDialogButton
-        v-for="button of buttonList"
-        :key="button.device.id"
-        :displayData="button"
-      />
+      <ActionButton
+          v-for="button of buttonList"
+          :key="button.device.id"
+          :displayData="button"/>
     </div>
   </v-container>
 </template>
 
 <script>
-import DeviceDialogButton from "./card/DeviceDialogButton.vue";
+import ActionButton from "@/components/ActionButton.vue";
 
 export default {
   props: ["buttonList"],
-  components: { DeviceDialogButton },
+  components: { ActionButton },
 };
 </script>
