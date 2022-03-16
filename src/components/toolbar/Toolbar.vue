6c061ea88c242b40689fe05e3c684098db319ed8
Sedrik Staack <sedrik.staack@sidion.de>
2022-03-16 09:35:54 +0100

    toolbar picture

--- src/components/toolbar/Toolbar.vue
+++ src/components/toolbar/Toolbar.vue
@@ -1,7 +1,15 @@
 <template>
   <v-container class="d-flex">
-    <NewButton/>
-    <SettingsButton class="ml-auto"/>
+    <router-link to="/">
+      <v-img
+          lazy-src="@/assets/logo.png"
+          max-height="40"
+          max-width="100"
+          src="@/assets/logo.png"
+      ></v-img>
+    </router-link>
+    <NewButton class="ml-auto mr-3"/>
+    <SettingsButton/>
   </v-container>
 </template>
 
